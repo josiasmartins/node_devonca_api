beb4979c4ea74fa7c81d932f1f26446e8b63357a
josias.m.caitano <josias.m.caitano@accenture.com>
2024-10-01 16:30:13 -0300

    chore: add library nodemon

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const routes = Router();
 
 routes.get("/user/", new UserController().getAll)
 routes.post("/user/", new UserController().createUser);
-routes.put("/user/:id", new UserController().updateUser)
-routes.delete("/user/:id", new UserController().deleteUser)
+routes.put("/user/:id", new UserController().updateUser);
+routes.delete("/user/:id", new UserController().deleteUser);
 
 module.exports = routes;
