bdef4f6c292d575b09c466eaf9483fd195cf7388
josias.m.caitano <josias.m.caitano@accenture.com>
2024-10-05 12:20:12 -0300

    refactor: change let to const in ReadKeysConfig

--- src/config/ReadKeysConfig.ts
+++ src/config/ReadKeysConfig.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 const path_key_public = process.env.PATH_KEY_PUBLIC || "../../../keys/private.pem";
 const path_key_private = process.env.PATH_KEY_PRIVATE || "../../../keys/public.pem";
 
-let key_private = fs.readFileSync(path_key_private, 'utf8');
-let key_public = fs.readFileSync(path_key_public, 'utf8');
+const key_private = fs.readFileSync(path_key_private, 'utf8');
+const key_public = fs.readFileSync(path_key_public, 'utf8');
 
 export { key_private, key_public };
